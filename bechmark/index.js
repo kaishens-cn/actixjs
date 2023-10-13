80d5387f893a2948c133c4ffdea02ad7b1efee7d
cherish <45481560+cherishs001@users.noreply.github.com>
2023-10-13 14:19:50 +0800

    fix: add keepalive

--- bechmark/index.js
+++ bechmark/index.js
@@ -1,7 +1,7 @@
 const actix = require("../index");
 
 actix.get('/', (req) => {
-    req.sendText("hello world");
+  req.sendText("hello world");
 });
 
 const host = '127.0.0.1:8080'
@@ -11,7 +11,7 @@ actix.start(host);
 const http = require('http');
 
 http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // 发送响应数据 "Hello World"
-    response.end('hello world');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  // 发送响应数据 "Hello World"
+  response.end('hello world');
 }).listen(8081);
